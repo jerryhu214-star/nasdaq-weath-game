355de9a10a9f88c3e9bbf834f38cea04f6762289
jerryhu214-star <jerryhu214@gmail.com>
2026-07-23 14:30:09 +0800

    Add mini-program: app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+App({
+  globalData: {
+    // 游戏链接 - 部署后替换为你的永久链接
+    // 例如: https://nasdaq-wealth-jump.surge.sh/wealth-jump.html
+    gameUrl: 'https://jerryhu214-star.github.io/nasdaq-weath-game/wealth-jump.html'
+  }
+})
